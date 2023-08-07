508722196f9d0079723a0ba3bcc43364349dd354
jojun0 <jojun01835@gmail.com>
2023-08-07 15:44:08 +0900

    삭제 추가

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -10,36 +10,55 @@ const ProductPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [product, setProduct] = useState(null);
-  const getProduct = () => {
-    axios
-      .get(`${API_URL}/products/${id}`)
-      .then((result) => {
-        setProduct(result.data.product);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  const [loading, setLoading] = useState(true);
+  const [isPurchaseDisabled, setPurchaseDisabled] = useState(false);
 
   useEffect(() => {
     getProduct();
   }, []);
 
+  const getProduct = () => {
+    axios
+      .get(`${API_URL}/products/${id}`)
+      .then((result) => {
+        setProduct(result.data.product);
+        setLoading(false);
+        setPurchaseDisabled(result.data.product.soldout === 1);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  };
+
   const onClickPurchase = () => {
     axios
       .post(`${API_URL}/purchase/${id}`)
-      .then((result) => {
+      .then(() => {
         message.info(`결제가 완료되었습니다.`);
         getProduct();
       })
       .catch((error) => {
-        message.error(`에러가 발생했습니다 ${error.message}`);
+        message.error(`에러가 발생했습니다: ${error.message}`);
       });
   };
-  console.log(product);
-  if (product === null) {
+
+  const onClickDelete = () => {
+    axios
+      .delete(`${API_URL}/products/${id}`)
+      .then(() => {
+        message.info(`제품이 삭제되었습니다.`);
+        navigate(-1); // 삭제 후, 뒤로 가기
+      })
+      .catch((error) => {
+        message.error(`에러가 발생했습니다: ${error.message}`);
+      });
+  };
+
+  if (loading) {
     return <h2>제품 상품 정보를 받고 있습니다.....</h2>;
   }
+
   return (
     <div className="product-wrap">
       <button onClick={() => navigate(-1)} id="back-btn">
@@ -54,8 +73,11 @@ const ProductPage = () => {
             <div id="name">{product.name}</div>
             <div id="price">{product.price}</div>
             <div id="crateAt">{dayjs(product.createdAt).format("YYYY MM DD")}</div>
-            <Button type="primary" danger className="payment" size="large" onClick={onClickPurchase} disabled={product.soldout === 1 ? true : false}>
+            <Button type="primary" danger className="payment" size="large" onClick={onClickPurchase} disabled={isPurchaseDisabled}>
               즉시 결제 하기
+            </Button>
+            <Button type="primary" danger className="delete" size="large" onClick={onClickDelete}>
+              제품 삭제하기
             </Button>
             <div id="description">{product.description}</div>
           </div>
