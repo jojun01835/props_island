6b80edb93a94022801a127bcff300565b0eafbd1
jojun0 <jojun01835@gmail.com>
2023-08-07 14:36:27 +0900

    add

--- src/config/constants.jsx
+++ src/config/constants.jsx
@@ -1,2 +1,1 @@
-export const API_URL =
-  process.env.NODE_ENV === "production" ? "https://port-0-props-island-server-eu1k2lll0aqkvm.sel4.cloudtype.app/" : "http://localhost:8080";
+export const API_URL = process.env.NODE_ENV === "production" ? "https://port-0-props-island-server-eu1k2lll0aqkvm.sel4.cloudtype.app" : "http://localhost:8080";
